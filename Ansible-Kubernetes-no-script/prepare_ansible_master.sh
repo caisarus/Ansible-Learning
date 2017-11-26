8a7cf8ae651d373bb29f4acfe7fb5e5e1c5e38b9
caisarus <cezar.alexandru.vasilescu@gmail.com>
2017-11-26 21:14:26 +0000

    Update prepare_ansible_master.sh

--- Ansible-Kubernetes-no-script/prepare_ansible_master.sh
+++ Ansible-Kubernetes-no-script/prepare_ansible_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Install ansible and update
-sudo yum install epel-release -y 
-sudo yum install ansible git vim -y 
+sudo yum install epel-release -y
+sudo yum install ansible git vim -y
 sudo yum update -y
 
 #Create ansible user and give ansible sudo access
@@ -12,10 +12,11 @@ sudo chown ansible:ansible /home/ansible
 sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 #Copy everything to ansible's user home directory
-mkdir /home/ansible/workspace && cp -r * /home/ansible/workspace && chown -R ansible:ansible /home/ansible/*
-if [ $? != 0 ] then:
-echo "Please verify that ansible user exists and folder not already created"
-exit
+cp -r * /home/ansible/workspace && chown -R ansible:ansible /home/ansible/*
+if [ $? != 0 ]
+then
+        echo "Please verify that ansible user exists and folder not already created"
+        exit
 fi
 
 
@@ -24,9 +25,9 @@ su - ansible
 cd /home/ansible/workspace
 cat /dev/zero | ssh-keygen -q -N ""
 
-echo "Host *" >> /home/ansible/workspace/.ssh/config
-echo "        StrictHostKeyChecking no" >> /home/ansible/workspace/.ssh/config
-chmod 400 /home/ansible/workspace/.ssh/config
+echo "Host *" >> /home/ansible/.ssh/config
+echo "        StrictHostKeyChecking no" >> /home/ansible/.ssh/config
+chmod 400 /home/ansible/.ssh/config
 
 echo "Preparation for ansible use is finished \
 Please use one touch script to set up the cluster"
